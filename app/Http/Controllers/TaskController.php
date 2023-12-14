23f576b7d4fac1f2520258b9ed1ba4ba4d1bdba8
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-12-14 13:37:47 +0100

    Initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::latest()->get();
+        return response()->json($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|unique:tasks,title|max:200',
+            'description' => 'required|max:2000',
+            'date' => 'required|date|after:today',
+        ]);
+        Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date
+        ]);
+        return response()->json([
+            "success" => "task create with success"
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'required|max:200',
+            'description' => 'required|max:2000',
+            'date' => 'required|date|after:today',
+        ]);
+        $task->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date
+        ]);
+        return response()->json([
+            "success" => "task update with success"
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+        $task->delete();
+        return response()->json([
+            "success" => "task delete with success"
+        ]);
+    }
+}
